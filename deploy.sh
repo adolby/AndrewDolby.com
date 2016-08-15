0a6a90f59ba13d025dfd2fbc88f7f3e61e0e646f
Andrew Dolby <andrewdolby@gmail.com>
2016-08-15 16:08:00 -0400

    Update Travis deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
+if [ "${TRAVIS_BRANCH}" != "master" ]
 then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  echo "This commit was made against ${TRAVIS_BRANCH}, not master! No deploy!"
   exit 0
 fi
 
@@ -16,11 +16,11 @@ git init
 git config user.name "Travis CI"
 git config user.email "andrewdolby@gmail.com"
 git config --global push.default simple
-git remote add upstream "https://${GH_TOKEN}@${GH_REF}"
+git remote add upstream "https://${GH_TOKEN}@github.com/adolby/adolby.github.io.git"
 
 echo "andrewdolby.com" > CNAME
 touch .
 
 git add -A .
 git commit -m "Deploy to GitHub Pages - ${TRAVIS_COMMIT}"
-git push --force upstream master >/dev/null 2>&1
+git push --force upstream master > /dev/null 2>&1
